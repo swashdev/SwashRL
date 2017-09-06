4ec13d3a4924bcefce04ec9cd7459fa63e459e2c
Philip Pavlick <philip@swash.link>
2017-09-06 19:53:44 -0400

    Consolidate RNG files
    To avoid confusion with ``std.random'', ``random.d'' is consolidated
    into ``dice.d''

--- src/dice.d
+++ src/dice.d
@@ -25,3 +25,52 @@ dicebag Dice( ubyte d, short m, int f, int c )
   dicebag r = { dice:d, modifier:m, floor:f, ceiling:c };
   return r;
 }
+
+import std.random, std.datetime;
+
+// Hi, my name is Lucky, and I'll be your random number generator today.
+static RandomGen Lucky;
+
+// rng maintenance //
+
+void set_seed( int s )
+{ Lucky = Random( s );
+}
+
+void seed()
+{ Lucky = Random( cast(int)core.stdc.time.time(null) );
+}
+
+// boolean dice
+
+// flip a coin
+bool flip()
+{ return (d2() == 1);
+}
+
+// the standard six-sided die //
+
+// roll a d6
+int d()
+{ return uniform( 1, 7, Lucky );
+}
+
+// roll a d6 with modifier
+int dm( byte mod )
+{ return (d() + mod);
+}
+
+// "special case" dice--the d10, d100, and d2 //
+
+// for true old-school, we're using the old-fashioned d10 fashioned out of a
+// d20 that has been numbered from 0 to 9 twice
+int td10()
+{
+  auto result = uniform( 1, 21, Lucky );
+  if( result > 9 )
+  { result -= 10;
+  }
+  return cast(int)result;
+}
+
+// a non-traditional d10 numbered from 1 to 10

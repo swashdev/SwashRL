fad03c6c795057d49f6ccc6c45b2d5b390829b82
Philip Pavlick <philip@swash.link>
2017-09-09 01:30:56 -0400

    Adding forgotten RNG functions
    For some reason git didn't import the functions in ``src/dice.d''
    properly, so I'm adding them back in now.

--- src/dice.d
+++ src/dice.d
@@ -76,3 +76,86 @@ int td10()
 }
 
 // a non-traditional d10 numbered from 1 to 10
+int d10()
+{
+  int result = td10();
+  return result == 0 ? 10 : result;
+}
+
+// again, going by tradition, use 2 d10s to determine the result of a d100
+// roll.  Zocchi, I love you, but the 100-sided die is a little ridiculous
+int d100()
+{
+  int t = td10(), u = td10();
+  if( t == 0 && u == 0 )
+  { return 100;
+  }
+  return (t * 10) + u;
+}
+
+// roll one 2-sided die
+int d2()
+{ return uniform( 1, 3, Lucky );
+}
+
+// roll one s-sided die
+int dn( int8 s )
+{
+  // if sides == 1, just return 1.  if sides == 0, return 0.
+  if( s < 2 )
+  { return s;
+  }
+  return uniform( 1, s + 1, Lucky );
+}
+
+// the complete roll function
+int roll( ubyte num, byte mod )
+{
+  if( num == 0 )
+  { return mod;
+  }
+
+  int result = d();
+
+  ubyte dice;
+  for( dice = num - 1; dice > 0; dice-- )
+  { result += d();
+  }
+
+  return result + mod;
+}
+
+int roll_x( ubyte num, byte mod, int floor, int ceiling )
+{
+  if( num == 0 )
+  { return mod;
+  }
+
+  dieroll_t result = 0;
+  uint8 dice;
+  for( dice = num - 1; dice > 0; dice-- )
+  { result += d();
+  }
+
+  if( result > MAXIMUM_DIE_ROLL )
+  { result = MAXIMUM_DIE_ROLL;
+  }
+  if( result < MINIMUM_DIE_ROLL )
+  { result = MINIMUM_DIE_ROLL;
+  }
+
+  return minmax( result, floor, ceiling ) + mod;
+}
+
+bool quickcheck( ubyte num, byte mod, int difficulty )
+{ return roll( num, mod ) <= difficulty;
+}
+
+bool quickcheck_x( ubyte num, byte mod, int difficulty,
+                   int floor, int ceiling )
+{ return roll_x( num, mod, floor, ceiling ) <= difficulty;
+}
+
+dieroll_t rollbag( dicebag dice )
+{ return roll_x( dice.dice, dice.modifier, dice.floor, dice.ceiling );
+}

cc5424f874ccaf0f3473021c677da0cf3e0a933b
Philip Pavlick <philip@swash.link>
2018-08-29 05:55:26 -0400

    Added autodoc comments to msg.d

--- src/msg.d
+++ src/msg.d
@@ -15,6 +15,9 @@
  * limitations under the License.
  */
 
+// msg.d:  Declares the global message buffers and functions used to handle
+// them
+
 import global;
 
 import std.string;
@@ -23,17 +26,60 @@ public import std.range.primitives : popFront;
 
 enum MESSAGE_BUFFER_LINES = 1;
 
-// `Messages' is used for new messages...
+/++
+ + The message queue
+ +
+ + This is a global `DList` used as a queue to store messages that are
+ + waiting to be output to the user.  This queue stores messages until the
+ + end of the player's and monsters' turns, whereupon `main` calls the message
+ + functions which empty this queue out.
+ +
+ + This queue is populated by the `message` function and is manipulated by
+ + the `pop_message` and `clear_messages` functions.
+ +
+ + See_Also:
+ +   <a href="#Message_history">Message_history</a>
+ +/
 DList!string Messages;
 
-// ...whereas `Message_history' is used for stored previous messages which can
-// be read by the P key prompt
+/++
+ + The message buffer
+ +
+ + This is a global array of `string`s which is used for storing the player's
+ + message history.  This array will be populated by a maximum of
+ + `MAX_MESSAGE_BUFFER` messages which can be read back to the player on
+ + demand.
+ +
+ + This array is populated by the `message` function.
+ +
+ + See_Also:
+ +   <a href="#Messages">Messages</a>
+ +/
 string[] Message_history;
 
+/++
+ + Clears the message queue
+ +/
 void clear_messages()
 { Messages.clear();
 }
 
+/++
+ + Pops the first message in the message queue
+ +
+ + This function gets the first message in `Messages` and removes it from the
+ + queue.  If `Messages` is empty, an empty string is returned.
+ +
+ + Each call of this function will cause the `string` at the front of
+ + `Messages` to be removed, unless `Messages` was already empty.
+ +
+ + See_Also:
+ +   <a href="#Messages">Messages</a>
+ +
+ + Returns:
+ +   The `string` which was at the front of `Messages` when the function was
+ +   called, or an empty `string` (`""`) if `Messages` was already empty.
+ +/
 string pop_message()
 {
   if( Messages.empty() )
@@ -45,6 +91,35 @@ string pop_message()
   return ret;
 }
 
+/++
+ + Adds a message to the message queue
+ +
+ + This function takes in a message and appends it at the back of the
+ + `Messages` queue and the `Message_history` array.  The message is formatted
+ + from a generic "args" template, allowing the message to be formatted during
+ + the function call.
+ +
+ + See_Also:
+ +   <a href="#Messages">Messages</a>,
+ +   <a href="#Message_history">Message_history</a>
+ +
+ + Examples:
+ +   ---
+ +   // Appends "Hello, world!" to the back of the message queue:
+ +   message( "Hello, world!" );
+ +   ---
+ +   ---
+ +   // Appends "The goobling attacks you!" to the back of the message queue:
+ +   message( "The %s attacks you!", "goobling" );
+ +   ---
+ +   ---
+ +   // Appends "You eat 3 hamburgers." to the back of the message queue:
+ +   message( "You eat %d hamburgers.", 3 );
+ +   ---
+ +
+ + Params:
+ +   args = Formatting arguments for the message to be appended to `Messages`
+ +/
 void message( T... )(T args)
 {
   // Append the message to the message buffer...

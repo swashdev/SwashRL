6d20f05225baf3f81df1d6897a763d1be16e3b43
Philip Pavlick <swashdev@pm.me>
2019-09-01 12:57:18 -0500

    Adjust room sizes again
    Hey, genius, when you add an odd number to an odd number you get an even
    number that breaks your odds-only coordinate system.

--- src/mapgen.d
+++ src/mapgen.d
@@ -24,8 +24,8 @@ import std.random;
 import global;
 
 // Some universal configuration variables:
-enum MIN_ROOM_X =  3, MIN_ROOM_Y = 3;
-enum MAX_ROOM_X = 17, MAX_ROOM_Y = 5;
+enum MIN_ROOM_X =  4, MIN_ROOM_Y = 4;
+enum MAX_ROOM_X = 18, MAX_ROOM_Y = 6;
 
 const int[4][12] SECTORS =
 [ [1, 19,  1,  7], [21, 39,  1,  7], [41, 59,  1,  7], [61, 79,  1,  7],

75d216341801281368bbce6677d6f9244cf7a843
Philip Pavlick <swashdev@pm.me>
2019-09-01 16:40:03 -0500

    Updated the version number

--- src/global.d
+++ src/global.d
@@ -50,7 +50,7 @@ enum NAME = "SwashRL";
  + number and the second is the three-digit revision number.  This number
  + is stored as a floating-point number.
  +/
-enum VERSION = 0.029;
+enum VERSION = 0.030;
 
 /++
  + The commit ID

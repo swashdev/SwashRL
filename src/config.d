1a08cb159f251edd3f2726cd122eff0239ba8bdb
Philip Pavlick <swashdev@pm.me>
2020-02-27 12:33:50 -0600

    Reduce redundancy in a clarification line
    Saying that an option only affects the SDL terminal interface is
    redundant if the option only appears in the SDL section of the
    configuration file, so instead a brief description of the curses
    behavior this is meant to emulate is given.

--- src/config.d
+++ src/config.d
@@ -88,9 +88,9 @@ enum TILE_HEIGHT = 16;
 // The width to use for each tile in pixels
 enum TILE_WIDTH = 8;
 
-// Whether to highlight the player in the game display.  This only has an
-// effect on the SDL virtual terminal display, as the curses terminal
-// highlights the player using the cursor.
+// Whether to highlight the player in the game display, as would normally
+// happen in a curses terminal when the cursor hovers over the player's
+// location.
 enum HILITE_PLAYER = true;
 
 // SECTION 2: ////////////////////////////////////////////////////////////////

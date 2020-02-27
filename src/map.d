fe8528c7d84b7cec045a0141425d5a0112dcf2a0
Philip Pavlick <swashdev@pm.me>
2020-02-27 15:00:15 -0600

    Remove autodoc comments from map.d
    As part of an initiative to render bloated autodoc comments obsolete and
    declutter the source code, autodoc comments in map.d have been replaced
    with simple clarifying comment lines.

--- src/map.d
+++ src/map.d
@@ -40,23 +40,7 @@ struct Room
 // Defines a struct ``Map'' which stores Map data, including Map Tiles and
 // Monsters and Items on the Map
 
-/++
- + The Map struct
- +
- + This struct defines a Map.
- +
- + It contains a two-layered array of `Tile`s, `t`, which represents Map
- + Tiles; a dynamic array of `Monst`s, `m`, which represents the Monsters on
- + the Map; a two-layered array of `Item`s, `i`, which represents the Items
- + contained on each Tile of the Map.
- +
- + The Map also contains an array of two `ubyte`s, `player_start`, which
- + represents where the player starts out when the Map is loaded.  This is
- + currently only used for opening save files.
- +
- + The Map also contains an array of nine `Room`s, `r`, which is not currently
- + used by any function.
- +/
+// A struct which stores map data
 struct Map
 {
   Tile[MAP_X][MAP_Y]   t; // `t'iles
@@ -71,19 +55,7 @@ struct Map
 
 }
 
-/++
- + Adds a Monster to the given Map.
- +
- + This function adds a given `Monst` to the given `Map`'s `m` array.
- +
- + See_Also:
- +   <a href="#remove_mon">remove_mon</a>
- +
- + Params:
- +   mp = A pointer to the Map which mn is to be added to.  mp will be changed
- +        by the function, hence why it needs to be a pointer.
- +   mn = The Monster to be added to mp.
- +/
+// Add a monster to the given map
 void add_mon( Map* mp, Monst mn )
 {
   size_t mndex = mp.m.length;
@@ -94,22 +66,7 @@ void add_mon( Map* mp, Monst mn )
   }
 }
 
-/++
- + Removes a Monster from the given Map.
- +
- + This function will remove the Monster given by index from `mp`'s `m` array.
- + The Monster is removed by overwriting it with all of the Monsters to the
- + right of it in the array and then truncating the length of the array.
- +
- + See_Also:
- +   <a href="#add_mon">add_mon</a>
- +
- + Params:
- +   mp    = A pointer to the Map which the Monster at index is to be removed
- +           from.  mp will be changed by the function, hence why it needs to
- +           be a pointer.
- +   index = The index in `mp.m` which is to be removed.
- +/
+// Remove a monster from the given map by index
 void remove_mon( Map* mp, uint index )
 {
   // To remove a Monster in a Map's mon array, move all Monsters that are

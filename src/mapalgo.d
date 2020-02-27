927b3840294dd1661fb305d8c93a1ff75870141b
Philip Pavlick <swashdev@pm.me>
2020-02-27 14:31:22 -0600

    Add an introductory comment to mapalgo.d

--- src/mapalgo.d
+++ src/mapalgo.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019 Philip Pavlick.  See '3rdparty.txt' for other
+ * Copyright (c) 2015-2020 Philip Pavlick.  See '3rdparty.txt' for other
  * licenses.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,6 +25,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+// mapalgo.d: defines functions related to map generation algorithms
 
 public import global;
 import std.random;

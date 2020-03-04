b3fb570eda8e79fbaf1b4118a1fc9df199fc6600
Philip Pavlick <swashdev@pm.me>
2020-03-04 12:39:08 -0600

    Add an introductory comment to tflags.d

--- src/tflags.d
+++ src/tflags.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2019 Philip Pavlick.  See '3rdparty.txt' for other
+ * Copyright (c) 2015-2020 Philip Pavlick.  See '3rdparty.txt' for other
  * licenses.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -25,6 +25,9 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+// tflags.d: defines flags used to mark special properties about a tile such
+// as hazards.
 
 import global;
 

e568f9ba8d667149d4edf17f6573005c7817eb9b
Philip Pavlick <swashdev@pm.me>
2020-03-08 17:46:47 -0500

    Totally overhaul color.d
    The way that colors work in SwashRL is far too messy and depends on
    large functions to interpret vague color codes.  Instead, we're going to
    start using classes to store generic color constructs which store color
    data for either interface.
    
    In order to facilitate this, all of the color definitions have been
    removed from color.d and moved to a new file, colors.d, where they have
    been rewritten to use this new color system.

--- src/color.d
+++ src/color.d
@@ -26,131 +26,188 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-// color.d: Defines aliases for colors that can be used by the curses or SDL
-// interfaces
+// color.d: Defines classes used by SwashRL to store color values for both the
+// curses and SDL terminal interfaces.
 
 import global;
 
-/////////////////
-// Color pairs //
-/////////////////
+// SECTION 1: ////////////////////////////////////////////////////////////////
+// Colors                                                                   //
+//////////////////////////////////////////////////////////////////////////////
 
-/*
- * Used to store color data
- *
- * This struct is passed into functions that display colored text to determine
- * what colors to use.
- *
- * The `fg` variable stores a flag defined in color.d which determines what
- * the foreground color is.
- *
- * The `reverse` variable determines if the foreground and background colors
- * should be swapped.
- *
- * Note that there is no variable which determines the background color; the
- * background is always black.
- */
-struct Color
+// A class used to store color data
+class Color
 {
-  uint fg;
-  // `reverse' determines if the foreground and background colors are
-  // reversed.
-  bool reverse;
+
+static if( CURSES_ENABLED )
+{
+  private short curses_color = 7;
+
+  public short get_curses_color()
+  { return curses_color;
+  }
+
+  public void set_curses_color( short curses_color_code = 7 )
+  { curses_color = curses_color_code;
+  }
+
+} /* static if( CURSES_ENABLED ) */
+
+static if( SDL_ENABLED )
+{
+  private SDL_Color sdl_color = SDL_Color( 162, 162, 162, 255 );
+
+  public SDL_Color get_sdl_color()
+  { return sdl_color;
+  }
+
+  public void set_sdl_color( int sdl_red = 162, int sdl_blue = 162,
+                             int sdl_red = 162, int sdl_opacity = 255 )
+  { sdl_color = SDL_Color( sdl_red, sdl_blue, sdl_red, sdl_opacity );
+  }
+
+  public void set_sdl_color( SDL_Color sdl_color_object )
+  { sdl_color = sdl_color_object;
+  }
+
+} /* static if( SDL_ENABLED ) */
+
+
+  // Initialize a `Color` object by specifying the curses color code and the
+  // RGB values for an SDL color.  As an additional option, the opacity value
+  // has also been included, but should only be used sparingly.
+  this( short curses_color_code, int sdl_red, int sdl_blue, int sdl_green,
+        int sdl_opacity = 255 )
+  {
+
+static if( CURSES_ENABLED )
+{
+    curses_color = curses_color_code;
 }
 
-// A "default" color used if no color is specified during a function call.
-enum CLR_DEFAULT = Color( CLR_GRAY, false );
+static if( SDL_ENABLED )
+{
+    sdl_color = SDL_Color( sdl_red, sdl_blue, sdl_green, sdl_opacity );
+}
+    
+  } /* this( short, int, int, int, bool?, int? ) */
 
-///////////////////
-// Color indexes //
-///////////////////
+}
 
-// These enums will store indexes which will allow the `CursesIO' and
-// `SDLTerminalIO' classes to determine what color to use in display
-// functions.
+// SECTION 2: ////////////////////////////////////////////////////////////////
+// Color Pairs                                                              //
+//////////////////////////////////////////////////////////////////////////////
 
-/* This is a "special case" color that will tell the terminal to adopt the
- * color of the current square or use the default color, whichever applies.
- * Deprecated: This flag is not used; instead `CLR_DEFAULT` is a default
- * parameter in functions which accept a `Color` value.
- */
-enum CLR_NONE        = 16;
-
-enum CLR_BLACK       =  0;
-enum CLR_RED         =  1;
-enum CLR_GREEN       =  2;
-enum CLR_BROWN       =  3;
-enum CLR_MAGENTA     =  5;
-enum CLR_CYAN        =  6;
-enum CLR_GRAY        =  7;
-
-// Note that CLR_BLUE has a different index from CLR_LITEBLUE, but it behaves
-// exactly the same; both the curses and SDL terminal interfaces will treat it
-// like they are the same color.  This is because dark blue is too dark to be
-// used to any significant degree on a dark background.
-enum CLR_BLUE        =  4;
-
-enum CLR_DARKGRAY    =  8;
-enum CLR_LITERED     =  9;
-enum CLR_LITEGREEN   = 10;
-enum CLR_YELLOW      = 11;
-enum CLR_LITEBLUE    = 12;
-enum CLR_LITEMAGENTA = 13;
-enum CLR_LITECYAN    = 14;
-enum CLR_WHITE       = 15;
-
-///////////////////////////////
-// Curses color pair indexes //
-///////////////////////////////
-
-// Indexes used to label color pairs for curses.  These enums refer directly
-// to the standard curses color enums, but label them specifically as
-// `CURSES_*` in order to disambiguate them from the SDL colors in the source
-// code.
-
-version( curses )
+class Color_Pair
 {
 
-enum CURSES_BLACK       =  COLOR_BLACK;
-enum CURSES_RED         =  COLOR_RED;
-enum CURSES_GREEN       =  COLOR_GREEN;
-enum CURSES_BROWN       =  COLOR_YELLOW;
-enum CURSES_BLUE        =  COLOR_BLUE;
-enum CURSES_MAGENTA     =  COLOR_MAGENTA;
-enum CURSES_CYAN        =  COLOR_CYAN;
-enum CURSES_GRAY        =  COLOR_WHITE;
+  private Color foreground, background;
 
-} // version( curses )
+  // Because of the way curses works, "bright" color values must be specified
+  // here, in the color pair, because the bright colors are created by
+  // applying the "bold" attribute to an existing color pair.  This does not
+  // apply to SDL.
+  private bool bold = false;
 
-///////////////////////
-// SDL color presets //
-///////////////////////
+  public bool get_bright()
+  { return bold;
+  }
 
-// Structs used to define standard colors used by the SDL terminal interface.
-// Note the parallel naming scheme with the `CLR_*` indexes above.
+  public void set_bright( bool bright )
+  { bold = bright;
+  }
 
-version( sdl )
+static if( CURSES_ENABLED )
 {
 
-enum SDL_DARKGRAY = SDL_Color(  64,  64,  64, 255 );
-enum SDL_RED      = SDL_Color( 128,   0,   0, 255 );
-enum SDL_GREEN    = SDL_Color(   0, 128,   0, 255 );
-enum SDL_BROWN    = SDL_Color( 150,  75,   0, 255 );
-enum SDL_MAGENTA  = SDL_Color( 128,   0, 128, 255 );
-enum SDL_CYAN     = SDL_Color(   0, 128, 128, 255 );
-enum SDL_GRAY     = SDL_Color( 162, 162, 162, 255 );
+  // Because curses refers to color pairs by an index, we need to store what
+  // the most recent index was, as well as the index of the current color
+  // pair.  Note that if `last_color_pair` is 0, that means that no color
+  // pairs were defined, because curses uses 0 as sort of a default color
+  // pair.
 
-enum SDL_BLACK       = SDL_Color(   0,   0,   0, 255 );
-enum SDL_LITERED     = SDL_Color( 255,   0,   0, 255 );
-enum SDL_LITEGREEN   = SDL_Color(   0, 255,   0, 255 );
-enum SDL_YELLOW      = SDL_Color( 255, 255,   0, 255 );
-enum SDL_LITEBLUE    = SDL_Color(   0,   0, 255, 255 );
-enum SDL_LITEMAGENTA = SDL_Color( 255,   0, 255, 255 );
-enum SDL_LITECYAN    = SDL_Color(   0, 255, 255, 255 );
-enum SDL_WHITE       = SDL_Color( 255, 255, 255, 255 );
+  private static short last_color_pair = 0;
+  private short curses_color_pair = 0;
 
-// An alias which defines `SDL_BLUE` the same as `SDL_LITEBLUE` (see the
-// deprecation note in `CLR_BLUE`
-enum SDL_BLUE = SDL_LITEBLUE;
+  public short get_last_color_pair()
+  { return last_color_pair;
+  }
 
-} // version( sdl )
+  public short get_color_pair()
+  { return curses_color_pair;
+  }
+
+} /* static if( CURSES_ENABLED ) */
+
+  public static bool color_pairs_defined()
+  {
+static if( CURSES_ENABLED )
+{
+    return (last_color_pair > 0);
+}
+else
+{
+    return false;
+}
+  } /* public static bool color_pairs_defined() */
+
+  public Color get_foreground()
+  { return foreground;
+  }
+
+  public void set_foreground( Color new_foreground_color )
+  { foreground = new_foreground_color;
+  }
+
+  public Color get_background()
+  { return background;
+  }
+
+  public void set_background( Color new_background_color )
+  { background = new_background_color;
+  }
+
+  // Initialize a `Color_Pair` using an existing `Color` (or two if we want to
+  // specify a background other than black).
+  this( Color foreground_color,
+        Color background_color = Color( 0, 0, 0, 0 ) )
+  {
+    foreground = foreground_color;
+    background = background_color;
+
+static if( CURSES_ENABLED )
+{
+
+    // Initialize a curses color pair and then store the resulting index in
+    // `curses_color_pair`.
+    last_color_pair++;
+    curses_color_pair = last_color_pair;
+
+    init_pair( curses_color_pair, foreground.get_curses_color(),
+               background.get_curses_color() );
+
+} /* static if( CURSES_ENABLED ) */
+
+  } /* this( Color, Color? ) */
+
+  // Initialize a new color pair by taking an existing color pair and making
+  // it "bright"
+  public static Color_Pair brighten( Color_Pair original_pair,
+                                     int sdl_red, int sdl_green, int sdl_blue,
+                                     int sdl_opacity = 255 )
+  {
+
+    Color_Pair new_pair = original_pair;
+
+    new_pair.set_bright( true );
+
+static if( SDL_ENABLED )
+{
+    new_pair.set_sdl_pair( sdl_red, sdl_green, sdl_blue, sdl_opacity );
+}
+
+    return new_pair;
+
+  } /* public static Color_Pair brighten( Color_Pair, int, int, int, int? ) */
+
+} /* class Color_Pair */
---
+++ src/colors.d
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2015-2020 Philip Pavlick.  See '3rdparty.txt' for other
+ * licenses.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the SwashRL project nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// colors.d: Defines colors and color pairs used by SwashRL.  See color.d for
+// the `Color` and `Color_Pair` classes.
+
+// SECTION 0: ////////////////////////////////////////////////////////////////
+// Color Indexes                                                            //
+//////////////////////////////////////////////////////////////////////////////
+
+// Because curses defines color pairs according to index values, we must
+// record them here or find useful substitutes.
+
+///////////////////////////////
+// Curses color pair indexes //
+///////////////////////////////
+
+// Indexes used to label color pairs for curses.  These enums refer directly
+// to the standard curses color enums, but label them specifically as
+// `CURSES_*` in order to disambiguate them from the SDL colors in the source
+// code.
+
+version( curses )
+{
+
+enum CURSES_DARK        =  COLOR_BLACK;
+enum CURSES_RED         =  COLOR_RED;
+enum CURSES_GREEN       =  COLOR_GREEN;
+enum CURSES_BROWN       =  COLOR_YELLOW;
+enum CURSES_BLUE        =  COLOR_BLUE;
+enum CURSES_MAGENTA     =  COLOR_MAGENTA;
+enum CURSES_CYAN        =  COLOR_CYAN;
+enum CURSES_GRAY        =  COLOR_WHITE;
+
+} // version( curses )
+else
+{
+
+// NOTE: these placeholder values are taken from the pdcurses source code
+
+enum CURSES_DARK    = 0;
+enum CURSES_RED     = 1;
+enum CURSES_GREEN   = 2;
+enum CURSES_BLUE    = 4;
+enum CURSES_BROWN   = 3;
+enum CURSES_MAGENTA = 5;
+enum CURSES_CYAN    = 6;
+enum CURSES_GRAY    = 7;
+
+}
+
+// SECTION 1: ////////////////////////////////////////////////////////////////
+// Colors                                                                   //
+//////////////////////////////////////////////////////////////////////////////
+
+// Define the standard set of colors.
+
+static Color CLR_DARK    = Color( CURSES_DARK,     64,  64,  64 );
+static Color CLR_RED     = Color( CURSES_RED,     128,   0,   0 );
+static Color CLR_GREEN   = Color( CURSES_GREEN,     0, 128,   0 );
+static Color CLR_BLUE    = Color( CURSES_BLUE,      0,   0, 255 );
+static Color CLR_BROWN   = Color( CURSES_BROWN,   150,  75,   0 );
+static Color CLR_MAGENTA = Color( CURSES_MAGENTA, 128,   0, 128 );
+static Color CLR_CYAN    = Color( CURSES_CYAN,      0, 128, 128 );
+static Color CLR_GRAY    = Color( CURSES_GRAY,    162, 162, 162 );
+
+// SECTION 2: ////////////////////////////////////////////////////////////////
+// Color Pairs                                                              //
+//////////////////////////////////////////////////////////////////////////////
+
+// Define the standard color pairs.
+
+static Color_Pair PAIR_DARK    = Color_Pair( CLR_DARK    );
+static Color_Pair PAIR_RED     = Color_Pair( CLR_RED     );
+static Color_Pair PAIR_GREEN   = Color_Pair( CLR_GREEN   );
+static Color_Pair PAIR_BLUE    = Color_Pair( CLR_BLUE    );
+static Color_Pair PAIR_BROWN   = Color_Pair( CLR_BROWN   );
+static Color_Pair PAIR_MAGENTA = Color_Pair( CLR_MAGENTA );
+static Color_Pair PAIR_CYAN    = Color_Pair( PAIR_CYAN   );
+static Color_Pair PAIR_GRAY    = Color_Pair( PAIR_GRAY   );
+
+// SECTION 3: ////////////////////////////////////////////////////////////////
+// The Full Color Set                                                       //
+//////////////////////////////////////////////////////////////////////////////
+
+// We're going to put all of the color pairs into an enum named `Colors` so
+// we can easily access them later.
+
+enum Colors
+{
+  Dark_Gray = PAIR_DARK;
+  Red       = PAIR_RED;
+  Green     = PAIR_GREEN;
+  Blue      = PAIR_BLUE;
+  Brown     = PAIR_BROWN;
+  Magenta   = PAIR_MAGENTA;
+  Cyan      = PAIR_CYAN;
+  Gray      = PAIR_GRAY;
+
+// Bright Colors /////////////////////////////////////////////////////////////
+
+  // Because of the way that curses works, "bright" colors can't be defined in
+  // the same way the standard colors were, because curses relies on color
+  // pairs.  Instead we're going to "brighten" the existing color pairs.
+
+  Black      = Color_Pair.brighten( PAIR_DARK,      0,   0,   0 );
+  Lite_Red   = Color_Pair.brighten( PAIR_RED,     255,   0,   0 );
+  Lite_Green = Color_Pair.brighten( PAIR_GREEN,     0, 255,   0 );
+  Lite_Blue  = Color_Pair.brighten( PAIR_BLUE,      0,   0, 255 );
+  Yellow     = Color_Pair.brighten( PAIR_BROWN,   255, 255,   0 );
+  Pink       = Color_Pair.brighten( PAIR_MAGENTA, 255,   0, 255 );
+  Lite_Cyan  = Color_Pair.brighten( PAIR_CYAN,      0, 255, 255 );
+  White      = Color_Pair.brighten( PAIR_GRAY,    255, 255, 255 );
+
+} /* enum Colors */

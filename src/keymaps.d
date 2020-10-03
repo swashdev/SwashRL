89f961658ddd7104e2af6bb2a9922cafbd8b1838
Philip Pavlick <swashdev@pm.me>
2020-10-03 17:01:41 -0500

    conform keymaps.d to the style guide

--- src/keymaps.d
+++ src/keymaps.d
@@ -37,11 +37,12 @@ import global;
 
 // An exception used to catch when the programmer has accidentally
 // overwritten one of the reserved key commands: Q, ?, v, and @
-class InvalidKeymapException : Exception
+class Invalid_Keymap_Exception : Exception
 {
-  this( string msg, string file = __FILE__, size_t line = __LINE__ )
-  { super( msg, file, line );
-  }
+    this( string msg, string file = __FILE__, size_t line = __LINE__ )
+    {
+        super( msg, file, line );
+    }
 }
 
 // SECTION 2: ////////////////////////////////////////////////////////////////
@@ -51,100 +52,84 @@ class InvalidKeymapException : Exception
 // Initializes a keymap from a list of key inputs.
 Move[char] keymap( string keylist = "" )
 {
-  // The standard keymap, which is being overwritten
-  char[] kl = "ykuhlbjn.ie,dP S".dup;
+    // The standard keymap, which is being overwritten
+    char[] keys = "ykuhlbjn.ie,dP S".dup;
 
-  // Check to make sure that the keylist will not overwrite any reserved
-  // commands
-  if( keylist.length > 0 )
-  {
-    foreach( c; 0 .. keylist.length )
+    // Check to make sure that the keylist will not overwrite any reserved
+    // commands
+    if( keylist.length > 0 )
     {
-      switch( keylist[c] )
-      {
-        default:
-          kl[c] = keylist[c];
-          break;
+        foreach( count; 0 .. keylist.length )
+        {
+            switch( keylist[count] )
+            {
+                default:
+                    keys[count] = keylist[count];
+                    break;
 
-        case 'Q':
-          throw new InvalidKeymapException(
-            "Keymap attempted to overwrite reserved command \'Q\': " ~
-            "reserved for \"quit\""
-          );
+                case 'Q':
+                    throw new Invalid_Keymap_Exception(
+                            "Keymap attempted to overwrite reserved command "
+                            ~ "\'Q\': reserved for \"quit\"" );
 
-        case '?':
-          throw new InvalidKeymapException(
-            "Keymap attempted to overwrite reserved command \'?\': " ~
-            "reserved for \"help\""
-          );
+                case '?':
+                    throw new Invalid_Keymap_Exception(
+                            "Keymap attempted to overwrite reserved command "
+                            ~ "\'?\': reserved for \"help\"" );
 
-        case 'v':
-          throw new InvalidKeymapException(
-            "Keymap attempted to overwrite reserved command \'v\': " ~
-            "reserved for \"version\""
-          );
+                case 'v':
+                    throw new Invalid_Keymap_Exception(
+                            "Keymap attempted to overwrite reserved command "
+                            ~ "\'v\': reserved for \"version\"" );
 
-        case '@':
-          throw new InvalidKeymapException(
-            "Keymap attempted to overwrite reserved command \'@\': " ~
-            "reserved for \"switch control schemes\""
-          );
-      } // switch( keylist )
-    } // foreach( c; 0 .. keylist.length )
-  } // if( keylist.length > 0 )
+                case '@':
+                    throw new Invalid_Keymap_Exception(
+                            "Keymap attempted to overwrite reserved command "
+                            ~ "\'@\': reserved for \"switch control schemes\""
+                            );
+            } // switch( keylist[count] )
+        } // foreach( count; 0 .. keylist.length )
+    } // if( keylist.length > 0 )
 
-  // Now we define the actual keymap:
-  Move[char] ret;
+    // Now we define the actual keymap:
+    Move[char] ret;
 
-  // Standard movement keys:
-  ret[ kl[ 0] ] = Move.northwest;
-  ret[ kl[ 1] ] = Move.north;
-  ret[ kl[ 2] ] = Move.northeast;
-  ret[ kl[ 3] ] = Move.west;
-  ret[ kl[ 4] ] = Move.east;
-  ret[ kl[ 5] ] = Move.southwest;
-  ret[ kl[ 6] ] = Move.south;
-  ret[ kl[ 7] ] = Move.southeast;
-  ret[ kl[ 8] ] = Move.wait;
+    // Standard movement keys:
+    ret[ keys[ 0] ] = Move.northwest;
+    ret[ keys[ 1] ] = Move.north;
+    ret[ keys[ 2] ] = Move.northeast;
+    ret[ keys[ 3] ] = Move.west;
+    ret[ keys[ 4] ] = Move.east;
+    ret[ keys[ 5] ] = Move.southwest;
+    ret[ keys[ 6] ] = Move.south;
+    ret[ keys[ 7] ] = Move.southeast;
+    ret[ keys[ 8] ] = Move.wait;
 
-  // Inventory management
-  ret[ kl[ 9] ] = Move.inventory;
-  ret[ kl[10] ] = Move.equipment;
-  ret[ kl[11] ] = Move.get;
-  ret[ kl[12] ] = Move.drop;
+    // Inventory management
+    ret[ keys[ 9] ] = Move.inventory;
+    ret[ keys[10] ] = Move.equipment;
+    ret[ keys[11] ] = Move.get;
+    ret[ keys[12] ] = Move.drop;
 
-  // Message management
-  ret[ kl[13] ] = Move.check_messages;
-  ret[ kl[14] ] = Move.clear_message;
+    // Message management
+    ret[ keys[13] ] = Move.check_messages;
+    ret[ keys[14] ] = Move.clear_message;
 
-  // Game management
-  ret[ kl[15] ] = Move.save;
+    // Game management
+    ret[ keys[15] ] = Move.save;
 
-version( none )
-{
-  import std.stdio : writeln;
+    // Finally, we check the length of the keymap.  If the keymap isn't the
+    // same length as `keys`, one of the inputs has been defined twice and the
+    // control scheme is broken.
+    if( ret.length != keys.length )
+    {
+        throw new Invalid_Keymap_Exception( "Invalid keymap defined " ~
+            cast(string)keys ~ "  Did you map the same key twice?" );
+    }
 
-  writeln( "Size of keylsit: ", keylist.length,          "." );
-  writeln( "Size of kl:      ", kl.length,              "." );
-  writeln( "Size of ret:     ", ret.length,             "." );
-  writeln( "Keylist input:   ", keylist,                "." );
-  writeln( "kl final value:  ", cast(string)kl,         "." );
-  writeln( "final value:     ", cast(string)(ret.keys()), "." );
-}
-
-  // Finally, we check the length of the keymap.  If the keymap isn't the same
-  // length as `kl', one of the inputs has been defined twice and the control
-  // scheme is broken.
-  if( ret.length != kl.length )
-  {
-    throw new InvalidKeymapException( "Invalid keymap defined " ~
-      cast(string)kl ~ "Did you map the same key twice?" );
-  }
-
-  // Return the keymap
-  return ret;
-
-} // int[char] keymap( string )
+    // Return the keymap
+    return ret;
+} // Move[char] keymap( string? )
 
 // SECTION 3: ////////////////////////////////////////////////////////////////
 // Static variables used to store keymaps                                   //

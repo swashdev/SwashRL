f27f43a4ae2204788fda01a1d85f397aa855ae42
Philip Pavlick <swashdev@pm.me>
2021-01-12 17:58:12 -0600

    update copyright notices for previous commits.
    The year in the copyright notices in files modified by the previous
    three commits have been updated to reflect the current year.

--- src/item.d
+++ src/item.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2020 Philip Pavlick.  See '3rdparty.txt' for other
+ * Copyright (c) 2017-2021 Philip Pavlick.  See '3rdparty.txt' for other
  * licenses.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
